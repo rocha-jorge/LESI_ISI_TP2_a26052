576605230d8d6cb7961913209defc02082ca4e8a
rocha-jorge <108589433+rocha-jorge@users.noreply.github.com>
2024-12-22 23:37:30 +0000

    controlador TransactionController

---
+++ BitcoinApp/Controllers/TransactionController .cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BitcoinApp.Services.Internal;
+using BitcoinApp.Models;
+using BitcoinApp.Services.Internal;
+
+namespace BitcoinApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly ITransactionService _transactionService;
+
+        public TransactionController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Transaction>> GetTransaction(int id)
+        {
+            var transaction = await _transactionService.GetTransactionByIdAsync(id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+            return Ok(transaction);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddTransaction([FromBody] Transaction transaction)
+        {
+            if (transaction == null)
+            {
+                return BadRequest();
+            }
+
+            await _transactionService.AddTransactionAsync(transaction.IdUser, transaction.TransactionType, transaction.Units, transaction.BtcTimeStamp);
+            return CreatedAtAction(nameof(GetTransaction), new { id = transaction.IdTransaction }, transaction);
+        }
+    }
+}
